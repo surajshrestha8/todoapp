10a0abb36d9292bc0becbfa48a34d5fb8a1a9e31
Lalit Thapa <mlalitthapa@gmail.com>
2022-02-17 22:28:27 +0545

    Setup react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Suspense } from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import './assets/styles/app.css';
 import BackdropLoader from './components/elements/loader/BackdropLoader';
+import queryClient from './http/queryClient';
 import Routes from './routes';
 
 function App() {
   return (
     <Suspense fallback={<BackdropLoader open={true} />}>
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Suspense>
   );
 }
