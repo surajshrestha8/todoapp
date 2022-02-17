0819b6e0a4e6ff406491a9df86b9eb25e6143745
Lalit Thapa <mlalitthapa@gmail.com>
2022-02-17 21:57:20 +0545

    Setup routes with layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,19 @@
 import { lazy } from 'react';
 import { Route, Routes as Wrapper } from 'react-router-dom';
+import AuthLayout from '../components/layout/AuthLayout';
+import GuestLayout from '../components/layout/GuestLayout';
 const HomePage = lazy(() => import('../pages/home'));
+const LoginPage = lazy(() => import('../pages/auth/login'));
 
 const Routes = () => {
   return (
     <Wrapper>
-      <Route path="/" element={<HomePage />} />
+      <Route element={<GuestLayout />}>
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+      <Route element={<AuthLayout />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
     </Wrapper>
   );
 };
