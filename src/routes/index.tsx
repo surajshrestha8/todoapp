7e13879ad37972f8fe0ccbdcd85203407e8d19f0
Lalit Thapa <mlalitthapa@gmail.com>
2022-02-18 21:54:51 +0545

    Add notification component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,27 @@
 import { lazy } from 'react';
 import { Route, Routes as Wrapper } from 'react-router-dom';
+import Snackbar from '../components/elements/feedback/Snackbar';
 import AuthLayout from '../components/layout/AuthLayout';
 import GuestLayout from '../components/layout/GuestLayout';
+import { useNotificationStore } from '../store/app.store';
 const HomePage = lazy(() => import('../pages/home'));
 const LoginPage = lazy(() => import('../pages/auth/login'));
 
 const Routes = () => {
+  const { message, type } = useNotificationStore();
+
   return (
-    <Wrapper>
-      <Route element={<GuestLayout />}>
-        <Route path="/login" element={<LoginPage />} />
-      </Route>
-      <Route element={<AuthLayout />}>
-        <Route path="/" element={<HomePage />} />
-      </Route>
-    </Wrapper>
+    <>
+      <Snackbar message={message || ''} type={type} />
+      <Wrapper>
+        <Route element={<GuestLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+      </Wrapper>
+    </>
   );
 };
 
