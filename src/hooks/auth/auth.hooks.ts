0586af141be1e8c12b9b34fc47ef6d37f92795cb
Lalit Thapa <mlalitthapa@gmail.com>
2022-02-18 21:55:17 +0545

    Authenticate users

--- src/hooks/auth/auth.hooks.ts
+++ src/hooks/auth/auth.hooks.ts
@@ -1,15 +1,41 @@
 import { useEffect, useState } from 'react';
+import { useMutation } from 'react-query';
+import http from '../../http';
+import { AUTH } from '../../http/endpoints';
+import { HttpError } from '../../interface/common.interface';
+import { useNotificationStore } from '../../store/app.store';
 import { useAuthStore } from '../../store/auth.store';
+
+export interface AuthRequest {
+  email: string;
+  password: string;
+}
 
 const useAuth = () => {
   const { accessToken, setAccessToken } = useAuthStore();
+  const { resetMessage, setErrorMessage } = useNotificationStore();
 
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!accessToken);
   useEffect(() => {
     setIsAuthenticated(!!accessToken);
   }, [accessToken]);
 
-  return { accessToken, isAuthenticated, setAccessToken };
+  const { isLoading, mutate: login } = useMutation(
+    (data: AuthRequest) => {
+      resetMessage();
+      return http.post(AUTH.LOGIN, data);
+    },
+    {
+      onSuccess: (response) => {
+        setAccessToken(response.token);
+      },
+      onError: (error: HttpError) => {
+        setErrorMessage(error?.response?.data?.message);
+      },
+    }
+  );
+
+  return { accessToken, isAuthenticated, setAccessToken, isLoading, login };
 };
 
 export default useAuth;
