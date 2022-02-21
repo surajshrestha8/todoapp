98f343b3116ec257ea282e184b56c61fcc8fdd03
Lalit Thapa <mlalitthapa@gmail.com>
2022-02-21 21:51:07 +0545

    Add icon and children

--- src/interface/common.interface.ts
+++ src/interface/common.interface.ts
@@ -8,6 +8,8 @@ export type HttpError = AxiosError;
 export interface RouteItem {
   id: string;
   path: string;
+  icon?: React.ReactNode;
   element: React.ReactNode;
   title?: string;
+  children?: Array<RouteItem>;
 }
