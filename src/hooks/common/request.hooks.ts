b9bf092127a0f436247fd0ecca4cfa6cf776d90f
Lalit Thapa <mlalitthapa@gmail.com>
2022-02-24 00:06:06 +0545

    Set alert messages

--- src/hooks/common/request.hooks.ts
+++ src/hooks/common/request.hooks.ts
@@ -1,7 +1,11 @@
+import { AxiosError } from 'axios';
 import { useEffect, useState } from 'react';
 import { useMutation, useQuery } from 'react-query';
 import http from '~/http';
+import queryClient from '~/http/queryClient';
 import { MutationOptions, PaginationProps } from '~/interface/common.interface';
+import { useNotificationStore } from '~/store/app.store';
+import { parseHttpError } from '~/utils/httpParser.utils';
 import { formatPagination } from '~/utils/pagination.utils';
 
 export function useFetchList<M = unknown>(key: string[], endpoint: string, query?: any) {
@@ -38,13 +42,35 @@ export function useFetchItem<M = unknown>(key: string[], endpoint: string) {
 
 export function useSaveRequest(endpoint: string, options?: MutationOptions) {
   const httpMethod = options?.editing ? 'put' : 'post';
+  const { setSuccessMessage, setErrorMessage } = useNotificationStore();
 
   return useMutation((data: any) => http[httpMethod](endpoint, data), {
     onSuccess: (response: any) => {
+      setSuccessMessage('Data successfully saved.');
       if (options?.onSuccess) {
         options.onSuccess(response);
       }
     },
-    onError: options?.onError,
+    onError: (error: AxiosError) => {
+      setErrorMessage(parseHttpError(error));
+      if (options?.onError) {
+        options.onError(error);
+      }
+    },
   });
 }
+
+export const useDeleteRequest = (
+  queryKey: string,
+  endpoint: (id: string | number) => string,
+  options?: MutationOptions
+) => {
+  const { setSuccessMessage } = useNotificationStore();
+  return useMutation((id: string | number) => http.delete(endpoint(id)), {
+    onSuccess() {
+      setSuccessMessage('Data successfully removed.');
+      queryClient.refetchQueries(queryKey, { exact: true });
+    },
+    onError: options?.onError,
+  });
+};
