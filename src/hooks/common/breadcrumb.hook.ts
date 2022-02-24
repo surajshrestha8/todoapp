28fe83f6a1163e5a4d7b280603412c6181eb561b
Lalit Thapa <mlalitthapa@gmail.com>
2022-02-24 21:19:17 +0545

    Fix dynamic routes

--- src/hooks/common/breadcrumb.hook.ts
+++ src/hooks/common/breadcrumb.hook.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { matchRoutes, useLocation, useParams } from 'react-router-dom';
+import { RouteItem } from '~/interface/common.interface';
 import authRoutes from '~/routes/auth.routes';
 
 interface BreadcrumbItem {
@@ -13,20 +14,35 @@ export const useBreadcrumbs = () => {
   const location = useLocation();
   const params = useParams();
   const [items, setItems] = useState<Array<BreadcrumbItem>>([]);
+  const [routes, setRoutes] = useState<Array<RouteItem>>([]);
 
   useEffect(() => {
-    const crumbs = authRoutes
-      .flatMap((item) => {
+    setRoutes(
+      authRoutes.flatMap((item) => {
         if (!item.children) {
           return [item];
         }
         return item.children;
       })
+    );
+  }, []);
+
+  useEffect(() => {
+    // Get the path pattern instead of actual path
+    // E.g. if current path is /admin/1/edit it will help us to find /admin/:id/edit
+    const pathPattern = matchRoutes(routes, location);
+    if (!pathPattern) {
+      return;
+    }
+
+    const routePath = pathPattern[0].route.path || '';
+
+    const crumbs = routes
       // Get all routes that contain the current one.
-      .filter(({ path }) => location.pathname.includes(path))
+      .filter(({ path }: RouteItem) => routePath.includes(path))
       // Swap out any dynamic routes with their param values.
       // E.g. "/pizza/:pizzaId" will become "/pizza/1"
-      .map(({ path, id, title }) => ({
+      .map(({ path, id, title }: RouteItem) => ({
         path: Object.keys(params).length
           ? Object.keys(params).reduce(
               (paramPath, param) => paramPath.replace(`:${param}`, params[param] || ''),
@@ -45,7 +61,7 @@ export const useBreadcrumbs = () => {
         isLast: index === crumbs.length - 1,
       }))
     );
-  }, [location, params]);
+  }, [location, params, routes]);
 
   return { items };
 };
